0eaaadc232ac51417421281dd1c2d45547e7cc81
Phlya <flyamer@gmail.com>
2024-01-10 15:54:11 +0100

    pinpoint the TA site

--- workflow/scripts/find_insertion_sites.py
+++ workflow/scripts/find_insertion_sites.py
@@ -1,11 +1,17 @@
+import numpy as np
 import pandas as pd
 import bioframe
+from Bio import SeqIO
 
+import os
 import argparse
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("--forward-peaks", "-f", type=str)
 argparser.add_argument("--reverse-peaks", "-r", type=str)
+argparser.add_argument("--sample-name", type=str, default=".")
+argparser.add_argument("--genome", "-g", type=str)
+argparser.add_argument("--insertion-seq", type=str, default="TA")
 argparser.add_argument("--output", "-o", type=str)
 args = argparser.parse_args()
 
@@ -22,7 +28,42 @@ r = pd.read_csv(
     names=["chrom", "start", "end", "name", "count"],
 )
 
+
 overlap = bioframe.overlap(f, r, how="inner")
-overlap = overlap[["chrom", "start_", "end"]]
-overlap.columns = ["chrom", "start", "end"]
-overlap.to_csv(args.output, sep="\t", index=False, header=False)
+if len(overlap) == 0:
+    open(args.output, "w").close()
+    exit()
+
+overlap["count"] = overlap["count"] + overlap["count_"]
+overlap = overlap.drop(columns=["count_"])
+
+start = overlap[["start", "start_"]].max(axis=1)
+end = overlap[["end", "end_"]].min(axis=1)
+
+strand = np.where(start == overlap["start"], "+", "-")
+overlap = overlap.assign(start=start, end=end, strand=strand)
+overlap = overlap.drop(columns=["start_", "end_"])
+
+pinpointed = []
+for record in SeqIO.parse(args.genome, "fasta"):
+    overlaps = overlap[overlap["chrom"] == record.id]
+    if len(overlaps) == 0:
+        continue
+    for i, row in overlaps.iterrows():
+        seq = record.seq[row["start"] : row["end"]].upper()
+        # Just finds the first TA
+        i = seq.find(args.insertion_seq)
+        if i == -1:
+            row["pinpointed"] = False
+        else:
+            row["pinpointed"] = True
+            row["start"] = row["start"] + i
+            row["end"] = row["start"] + 1
+        pinpointed.append(row)
+pinpointed = pd.DataFrame(pinpointed)
+pinpointed["name"] = args.sample_name
+pinpointed = pinpointed[
+    ["chrom", "start", "end", "name", "count", "strand", "pinpointed"]
+]
+
+pinpointed.to_csv(args.output, sep="\t", index=False, header=False)
